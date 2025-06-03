6263da0d2711bd5d9be53e5e148b9139f5b9d50e
ashimkarki7 <35464231+ashimkarki7@users.noreply.github.com>
2025-06-03 23:35:34 +0545

    Project Configuration

---
+++ src/component/login/slice.ts
@@ -0,0 +1,44 @@
+import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
+
+export interface User {
+  id: string;
+  password: string;
+}
+
+interface UserState {
+  users: User[];
+  currentUser: string | null;
+}
+
+const initialState: UserState = {
+  users: [],
+  currentUser: null,
+};
+
+export const userSlice = createSlice({
+  name: "userSlice",
+  initialState,
+  reducers: {
+    signUp(state, action: PayloadAction<User>) {
+      state.users.push(action.payload);
+    },
+    signIn(state, action: PayloadAction<{ id: string; password: string }>) {
+      const user = state.users.find(
+        (u) =>
+          u.id === action.payload.id && u.password === action.payload.password,
+      );
+      if (user) {
+        state.currentUser = user.id;
+        localStorage.setItem("auth_token", user.id);
+      }
+    },
+    signOut(state) {
+      state.currentUser = null;
+      localStorage.removeItem('auth_token');
+      localStorage.removeItem('persist:leadData');
+    },
+  },
+});
+
+export const { signUp, signIn, signOut } = userSlice.actions;
+export default userSlice.reducer;
---
+++ src/store/store.ts
@@ -0,0 +1,45 @@
+import { configureStore } from "@reduxjs/toolkit";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+import userReducer from '@/component/login/slice.ts';
+import { combineReducers } from "redux";
+
+
+const userPersistConfig = {
+  key: "user",
+  storage,
+};
+
+const rootReducer = combineReducers({
+  user: persistReducer(userPersistConfig, userReducer),
+});
+
+export const store = configureStore({
+  reducer: {
+    rootReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
+export const clearPersistedState = () => {
+  persistor.purge();
+};
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
